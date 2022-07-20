67d0e15f699df617cd420b5af53f07be8790d05b
mardelmariam <56597132+mardelmariam@users.noreply.github.com>
2022-07-20 12:17:28 -0500

    Detection of dark blobs
    Classic CV algorithms

--- blob_detection.py
+++ blob_detection.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-"""!@brief Detección de blobs en imagen difusa
+"""!@brief Detección de blobs oscuros en imagen difusa
 
 @author: Maryam del Mar Correa
-@version 1.0
+@version 1.1
 @date 2021
 
 """
@@ -21,14 +21,12 @@ from scipy import signal
 #%%
 
 def simple_threshold(arr, val, mode):
-    
         
     """!@brief Genera una nueva imagen, en la que todos los píxeles por encima
     de un umbral se conservan. La imagen debe estar en escala de grises
 
     @param[in] arr Arreglo correspondiente a la imgen a procesar
-    @param[in] val1 Umbral inferior
-    @param[in] val2 Umbral superior
+    @param[in] val Umbral inferior
     @parain[in] mode Fondo: 'black' o 'white'
     @return thresh_img Imagen generada
     """
@@ -71,6 +69,23 @@ def generate_gaussian_window(k_size, a, b, std, color):
 
     return gkern2d
 
+    
+#%%
+
+def adjust_brightness_contrast(img_arr, alpha, beta):
+    
+    """!@brief Realce de contraste y brillo de la imagen
+    
+    @param[in] img_arr Imagen a modificar
+    @param[in] alpha Factor de contraste
+    @parain[in] beta Factor de brillo
+    @return arr Imagen modificada
+
+    """
+    arr = (img_arr + beta)*alpha
+    return arr
+
+
 #%% - Cargar imagen general a procesar
 
 img_path = "ndvi_sample.tif"
@@ -91,61 +106,42 @@ for i in range (0, img.shape[0]):
 plt.figure()
 plt.gcf().set_dpi(1200)
 plt.imshow(img_sample, cmap='gray', vmin=0, vmax=255)
+
         
 #%% - Detección de puntos oscuros
 
-img_kernel = generate_gaussian_window(12, 96, 255, 4, 'light')
-img_process1 = cv2.morphologyEx(img_sample, cv2.MORPH_OPEN, img_kernel)
-
-DARK_SPOT_THRESHOLD = 96 
-thresh_img = simple_threshold(img_process1, DARK_SPOT_THRESHOLD, 'black')
-
+img_kernel = generate_gaussian_window(9, 40, 245, 1.3, 'light')
+        
+img_process1 = cv2.filter2D(img_sample/255.0, -1, img_kernel/255.0)
+    
+thresh_img = simple_threshold(img_process1, 31, 'black')
+   
 contours = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
+            
 img_result = np.zeros((thresh_img.shape[0], thresh_img.shape[1], 3), dtype='uint8')
-    
+            
 for i in range (0, img_result.shape[0]):
     for j in range (0, img_result.shape[1]):
         img_result[i,j,:] = img_sample[i,j]
-    
+            
 for c in contours[0]:
     a = cv2.contourArea(c) 
     p = cv2.arcLength(c,True) 
     ci = p**2/(4*np.pi*a) if a!=0 else 0
-    if ci > 1.1: # Evaluar circularidad
+    if ci > 0.7: # Evaluar circularidad
         x, y, w, h = cv2.boundingRect(c)
-        if w>11 and h>11 and w<46 and h<60:
+        if w>11 and h>11 and w<49 and h<49:
             cv2.rectangle(img_result, (x, y), (x+w, y+h), (255, 0, 0, 0), 2)
-    else:
-        pass
-    
-#%% - Detección de puntos claros
-    
-img_process2 = 255 - img_process1
-
-black_kernel = generate_gaussian_window(20, 150, 255, 7, 'dark')
-img_kernel1 = cv2.filter2D(1-img_sample/255, -1, kernel=black_kernel/255, borderType=cv2.BORDER_REPLICATE)
-    
-thresh_img = simple_threshold(img_kernel1, 96, 'white')
-    
-contours = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
-for c in contours[0]:
-    a = cv2.contourArea(c) 
-    p = cv2.arcLength(c,True) 
-    ci = p**2/(4*np.pi*a) if a!=0 else 0
-    if ci > 1.1: # Evaluar circularidad
-        x, y, w, h = cv2.boundingRect(c)
-        if w>11 and h>11 and w<80 and h<80:
-            cv2.rectangle(img_result, (x, y), (x+w, y+h), (255, 0, 0, 0), 2)
-    else:
-        pass
-    
-plt.figure()
+        else:
+            pass
+        
+fig = plt.figure()
+fig.suptitle("Detección de puntos oscuros", x=0.5, y=0.75)
 plt.gcf().set_dpi(1200)
 plt.subplot(1,3,1)
-plt.imshow(255-img_sample, cmap='gray', vmin=0, vmax=255)
+plt.imshow(img_process1, cmap='gray', vmin=0, vmax=255)
 plt.subplot(1,3,2)
 plt.imshow(thresh_img, cmap='gray', vmin=0, vmax=255)
 plt.subplot(1,3,3)
 plt.imshow(img_result, cmap='gray', vmin=0, vmax=255)
+    
